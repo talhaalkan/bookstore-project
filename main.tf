9b800ae0b6d7e0466b1695dc648641505ea4a722
talhaalkan <105417379+talhaalkan@users.noreply.github.com>
2022-11-21 00:37:20 +0300

    main.tf file was added

---
+++ main.tf
@@ -0,0 +1,87 @@
+//This Terraform Template creates 1 Docker Machines on EC2 Instances
+//Docker-compose Machines will run on Amazon Linux 2 with custom security group
+//allowing SSH (22) and HTTP (80) connections from anywhere.
+//User needs to select appropriate key name when launching the instance.
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "bookstore-server" {
+  ami                    = "ami-02e136e904f3da870"
+  instance_type          = "t2.micro"
+  key_name               = "talha"
+  vpc_security_group_ids = [aws_security_group.sec-gr.id]
+  tags = {
+    Name = "Web Server of Bookstore"
+  }
+  user_data = <<-EOF
+              #! /bin/bash
+              yum update -y
+              yum install git -y
+              amazon-linux-extras install docker -y
+              systemctl start docker
+              systemctl enable docker
+              usermod -a -G docker ec2-user
+              curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" \
+              -o /usr/local/bin/docker-compose
+              chmod +x /usr/local/bin/docker-compose         
+              hostnamectl set-hostname "docker-compose-bookstore-server"
+              cd /home/ec2-user
+              git clone https://github.com/talhaalkan/bookstore-project.git
+              cd /home/ec2-user/bookstore-project
+              docker build -t talha/bookstore:latest .
+              docker-compose up -d
+              EOF
+}
+
+resource "aws_security_group" "sec-gr" {
+  name = "bookstore-sec-group"
+  tags = {
+    Name = "bookstore project security group"
+  }
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    protocol    = "tcp"
+    to_port     = 5000
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = -1
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "docker-compose-bookstore-public-ip" {
+  value = aws_instance.bookstore-server.public_ip
+}
+
+output "bookstore-website" {
+  value = "http://${aws_instance.bookstore-server.public_dns}"
+}
